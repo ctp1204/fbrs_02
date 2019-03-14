82ed36530d2d21e2113530d5c9193cbee20435ee
ctp1204 <lqcuong.qt@gmail.com>
2019-03-14 08:40:16 +0700

    follow_follower

--- db/schema.rb
+++ db/schema.rb
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(version: 2019_03_15_022213) do
     t.string "activation_digest"
     t.boolean "activated"
     t.datetime "activated_at"
+
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
